fcbf8512f212200eed93736a3764eafaed9254d8
Dmitriy-mars <125693843+Dmitriy-mars@users.noreply.github.com>
2023-04-11 16:12:11 +0600

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+function openMenu(){
+    document.getElementById("sidebar").classList.toggle('active');
+    document.getElementById("bodye").classList.toggle('body-go');
+}
+
+
+function closeMenu(){
+    document.getElementById("sidebar").classList.toggle('active');
+    document.getElementById("bodye").classList.toggle('body-go');
+}
